08b35ed832f63eda36080bd3e723240468a4cb85
Kamba1a <headcr@bk.ru>
2019-06-10 23:06:47 +0300

    Урок 3

---
+++ Lesson3/Game_Asteroids/FarAsteroid.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+
+namespace Game_Asteroids
+{
+    /// <summary>
+    /// Объект "Далекий астероид"
+    /// </summary>
+    class FarAsteroid : BaseObject
+    {
+        public FarAsteroid(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+        }
+
+        /// <summary>
+        /// Отрисовка объекта
+        /// </summary>
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.FillEllipse(Brushes.Wheat, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+    }
+}
---
+++ Lesson3/Game_Asteroids/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+Ольга Назарова
+Задания:
+1.	Добавить космический корабль, как описано в уроке.
+2.	Доработать игру «Астероиды»:
+    a.	Добавить ведение журнала в консоль с помощью делегатов;
+    b.	* добавить это и в файл.
+3.	Разработать аптечки, которые добавляют энергию.
+4.	Добавить подсчет очков за сбитые астероиды.
+5.	* Добавить в пример Lesson3 обобщенный делегат.
+*/
+
+namespace Game_Asteroids
+{
+    static class Program
+    {
+        //[STAThread]
+        static void Main()
+        {
+            //Form form = new Form
+            //{
+            //    Width = Screen.PrimaryScreen.Bounds.Width,
+            //    Height = Screen.PrimaryScreen.Bounds.Height
+            //};
+
+            Form form = new Form();
+            form.Width = 800;
+            form.Height = 600;
+
+            Game.Init(form);
+            form.Show();
+            Game.Draw();
+            Application.Run(form);
+        }
+    }
+}
