cac79f1c07362bbc53090e52677194c720a5fca8
Kamba1a <headcr@bk.ru>
2019-06-13 23:31:30 +0300

    Урок 4

---
+++ Lesson4/Game_Asteroids/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+Ольга Назарова
+
+Задание 1.	Добавить в программу коллекцию астероидов. Как только она заканчивается (все астероиды сбиты), формируется новая коллекция, в которой на один астероид больше.
+*/
+
+namespace Game_Asteroids
+{
+    static class Program
+    {
+        //[STAThread]
+        static void Main()
+        {
+            //Form form = new Form
+            //{
+            //    Width = Screen.PrimaryScreen.Bounds.Width,
+            //    Height = Screen.PrimaryScreen.Bounds.Height
+            //};
+
+            Form form = new Form();
+            form.Width = 800;
+            form.Height = 600;
+
+            Game.Init(form);
+            form.Show();
+            Game.Draw();
+            Application.Run(form);
+        }
+    }
+}
---
+++ Lesson4/Game_Asteroids/Ship.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+
+namespace Game_Asteroids
+{
+    /// <summary>
+    /// Объект "Корабль"
+    /// </summary>
+    class Ship : BaseObject
+    {
+        public static event Message MessageDie;
+
+        /// <summary>
+        /// Изображение объекта
+        /// </summary>
+        Image _img;
+
+        /// <summary>
+        /// Жизнь корабля
+        /// </summary>
+        public int HP { get; private set; } = 100;
+
+        public Ship (Point pos, Point dir, Size size, string imgFilePath) : base(pos, dir, size)
+        {
+            _img = Image.FromFile(imgFilePath);
+        }
+
+        /// <summary>
+        /// Отрисовка корабля
+        /// </summary>
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawImage(_img, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+
+        /// <summary>
+        /// Уменьшение жизни корабля
+        /// </summary>
+        /// <param name="n"></param>
+        public void HPLow(int n)
+        {
+            Game.WriteLog?.Invoke($"Корабль получил урон на {n} HP");
+            HP = HP - n;
+        }
+
+        /// <summary>
+        /// Увеличение жизни корабля
+        /// </summary>
+        /// <param name="n"></param>
+        public void HPHigh(int n)
+        {
+            if (HP+n >= 100) n=100-HP;
+            HP = HP + n;
+            Game.WriteLog?.Invoke($"Корабль восстановил {n} HP");
+        }
+
+        /// <summary>
+        /// Обработка нажатия клавиши "Up"
+        /// </summary>
+        public void Up()
+        {
+            if (Pos.Y > 0) Pos.Y = Pos.Y - Dir.Y;
+        }
+
+        /// <summary>
+        /// Обработка нажатия клавиши "Down"
+        /// </summary>
+        public void Down()
+        {
+            if (Pos.Y < Game.Height) Pos.Y = Pos.Y + Dir.Y;
+        }
+
+        /// <summary>
+        /// Уничтожение корабля
+        /// </summary>
+        public void Die()
+        {
+            Game.WriteLog?.Invoke("Корабль уничтожен");
+            MessageDie?.Invoke();
+        }
+    }
+}
